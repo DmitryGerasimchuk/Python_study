57f0c1e1c720e80c589facf84d86cdeef296b051
Dmitry_Arhwolf <124234659+DmitryGerasimchuk@users.noreply.github.com>
2024-06-28 21:24:39 +0300

    Задача по отработке темы "Кортежи"

---
+++ task_tuple_1.py
@@ -0,0 +1,18 @@
+# Задача:
+
+# 1. Создайте картеж с разными типами элементов: 
+my_info = ('Dima', 'Gerasimchuk', 37, None, False)
+# print(my_info)
+# print(type(my_info))
+
+# 2. Создайте второй картеж: 
+my_names = ('Dmitry', 'Dimas', 'Dmitry Leonidovich')
+# print(my_names)
+# print(type(my_names))
+
+# 3. Объедините два картежа: 
+new_my_info = my_info + my_names
+# print(type(new_my_info))
+
+# 4. Выведите результат в терминал:
+print(new_my_info)
